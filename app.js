95b81ca02407f5b0ab5cb6361b50a5545b143cd3
Diego Rodriguez <diegorodriguez@plyzer.com>
2018-12-08 12:16:45 +0100

    Added the functionality of handling a continuous stream of data and created a basic animaiton for it.

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 let mainContainer = d3.select("#mainContainer");
 
-mainContainer.selectAll('div')
-    .exit().remove()
-    .data([4, 8, 15, 16, 23, 42])
-    .enter().append("div")
-    .html((d) => {
-        return "<b>D3 String</b>: Element number: " + d + "!";
-    });
+const SVG_WIDTH = 500;
+const SVG_HEIGHT = 300;
+
+// Initialize SVG container
+
+mainContainer.select('svg')
+    .attr('width', SVG_WIDTH)
+    .attr('height', SVG_HEIGHT);
+
+setInterval(() => {
+    let random_radius = Math.random() * 100;
+    let svg_container = mainContainer.select('svg');
+    let mainCircle = svg_container.selectAll('circle');
+    if (mainCircle.empty()) {
+        mainCircle.data([random_radius])
+            .enter()
+            .append('circle')
+            .attr('cx', random_radius)
+            .attr('cy', random_radius)
+            .attr('r', random_radius)
+            .attr('fill', 'purple')
+            .merge(svg_container)
+    } else {
+        console.log("Trying to update circle.");
+        mainCircle.data([random_radius])
+        .each(function (datum) {
+            let node = d3.select(this);
+            node.transition().attr('r', random_radius)
+        })
+    }
+
+    svg_container.exit().remove();
+}, 500);
